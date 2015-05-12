dfcbe235afe3a21419d8c630ce0d1cd1ba8043fe
Pedroma Gómez-Portillo <pedroma-gomezp@users.noreply.github.com>
2015-05-12 19:56:54 +0200

    Update gymkhana.py

--- gymkhana.py
+++ gymkhana.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 "Usage: sudo python3 {0}"
 
+#*********************************************************************
+# Author: Pedro-M. Gómez-Portillo López    pedroma.almagro@gmail.com
+#
+# You can redistribute and/or modify this file under the terms of the
+# GNU General Public License ad published by the Free Software
+# Foundation, either version 3 of the License, or (at your option)
+# and later version. See <http://www.gnu.org/licenses/>.
+#
+# This file is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.  
+# *********************************************************************/
 
 #IMPORTS----------------------------------------------------------------------------------
 import sys
